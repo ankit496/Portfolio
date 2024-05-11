c9a7da2f6c10aa40d91574ab91825254ca27f23c
ankit496 <akankit496@gmail.com>
2024-05-11 08:47:11 +0530

    changed file path2

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -112,7 +112,7 @@ function Skills() {
                                 </p>
                             </div>
                             <div className='bg-black shadow-xl shadow-gray-600 transition-transform transition-duration-3000 hover:scale-105'>
-                                <img src="\assets\redux.png" className="w-20 mx-auto mb-4 mt-4" alt="" />
+                                <img src="/assets/redux.png" className="w-20 mx-auto mb-4 mt-4" alt="" />
                                 <p className='text-white text-lg text-center'>
                                     Redux
                                 </p>
